6f98e92dda3178e32f0f362abd98ca13c3298fdb
geezlothbrok <giddyohene5@gmail.com>
2026-03-21 12:27:26 +0000

    chore: fix gitignore and remove tracked node_modules

--- api/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createNewUser } from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/signup", createNewUser);
-
-export default router;
